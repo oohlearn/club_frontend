2b864c8463ad331283fae173bdc7a71aeed0e184
ooh <learnooh@gmail.com>
2024-10-01 16:06:29 +0800

    pagination done

--- src/pages/Activities/ActivitiesPage.jsx
+++ src/pages/Activities/ActivitiesPage.jsx
@@ -1,37 +1,41 @@
 import { Space, Row } from "antd";
 import ActivityComponent from "./ActivityComponent";
-import styled from "styled-components";
 import SearchBar from "../../components/SearchBar";
 import TitleComponent from "../../components/TitleComponent";
 import PaginationComponent from "../../components/Pagenation";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-// TODO get
-// TODO ticket
-const StylePagination = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 35px;
-`;
+const apiUrl = process.env.REACT_APP_API_URL;
 
 function Activities() {
-  const [eventData, setActivitiesData] = useState([]);
-  const apiUrl = process.env.REACT_APP_API_URL;
+  const [eventData, setEventsData] = useState([]);
+  const [pageSize, setPageSize] = useState(8);
+  const [loading, setLoading] = useState(true);
+  const [totalItems, setTotalItems] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const getVideosData = async () => {
+  const getActivitiesData = async (page, size) => {
     try {
-      const response = await axios.get(`${apiUrl}activity/events/`);
-      setActivitiesData(response.data.events);
+      const response = await axios.get(`${apiUrl}activity/events/?page=${page}&page_size=${size}`);
+      setEventsData(response.data.events);
+      setTotalItems(response.data.total); // 假設 API 返回總項目
       console.log(response.data.events);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
+  const handlePageChange = (page, size) => {
+    setCurrentPage(page);
+    setPageSize(size);
+  };
+
   useEffect(() => {
-    getVideosData();
-  }, []);
+    getActivitiesData(currentPage, pageSize);
+  }, [currentPage, pageSize]);
+
   return (
     <>
       <TitleComponent label="近期活動" />
@@ -42,9 +46,12 @@ function Activities() {
           <ActivityComponent eventData={eventData} />
         </Row>
       </Space>
-      <StylePagination>
-        <PaginationComponent />
-      </StylePagination>
+      <PaginationComponent
+        current={currentPage}
+        pageSize={pageSize}
+        total={totalItems}
+        onChange={handlePageChange}
+      />
     </>
   );
 }

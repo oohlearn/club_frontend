4c4b13be59f3015ab543c2748034dfdb689df34c
ooh <learnooh@gmail.com>
2024-10-02 16:37:17 +0800

    search and date filter done

--- src/pages/Videos/VideosPage.jsx
+++ src/pages/Videos/VideosPage.jsx
@@ -11,17 +11,29 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 function Videos() {
   const [videosData, setVideosData] = useState([]);
-  const [pageSize, setPageSize] = useState(3); // 每頁顯示數量
+  const [pageSize, setPageSize] = useState(7); // 每頁顯示數量
   const [loading, setLoading] = useState(true);
   const [totalItems, setTotalItems] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const getVideosData = async (page, size) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [dateRange, setDateRange] = useState([null, null]);
+
+  const getVideosData = async (page, size, term = "", startDate = null, endDate = null) => {
     try {
-      const response = await axios.get(
-        `${apiUrl}information/videos/?page=${page}&page_size=${size}`
-      );
-      console.log(response.data);
+      let url = `${apiUrl}information/videos/?page=${page}&page_size=${size}`;
+      if (term) {
+        url += `&search=${encodeURIComponent(term)}`;
+      }
+      // 檢查 startDate 和 endDate，並確保它們是 moment 對象或字符串
+      if (startDate) {
+        url += `&start_date=${startDate.format("YYYY-MM-DD")}`;
+      }
+
+      if (endDate) {
+        url += `&end_date=${endDate.format("YYYY-MM-DD")}`;
+      }
+      const response = await axios.get(url);
       setVideosData(response.data.videos);
       setTotalItems(response.data.total); // 假設 API 返回總項目
     } catch (error) {
@@ -34,15 +46,26 @@ function Videos() {
     setCurrentPage(page);
     setPageSize(size);
   };
+
+  const handleSearch = (value) => {
+    setSearchTerm(value);
+    setCurrentPage(1);
+  };
+
+  const handleDateRangeChange = (dates) => {
+    setDateRange(dates);
+    setCurrentPage(1);
+  };
   useEffect(() => {
-    getVideosData(currentPage, pageSize);
-  }, [currentPage, pageSize]);
+    getVideosData(currentPage, pageSize, searchTerm, dateRange[0], dateRange[1]);
+  }, [currentPage, pageSize, searchTerm, dateRange[0], dateRange[1]]);
+
   return (
     <>
       <TitleComponent label="精華影片" />
 
       <Space direction="vertical" size="large">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} onDateRangeChange={handleDateRangeChange} />
         <Row>
           <VideoComponent videosData={videosData} />
         </Row>
